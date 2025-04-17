853915035fd8b0935c5eb8dd070c364b2e15a1db
Selcan <selcantaylan@gmail.com>
2025-04-17 21:52:46 +0300

    Add files via upload

---
+++ örnek1/örnek1/Form1.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace örnek1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnArtı = new System.Windows.Forms.Button();
+            this.btnEksi = new System.Windows.Forms.Button();
+            this.btnÇarpma = new System.Windows.Forms.Button();
+            this.btnBölme = new System.Windows.Forms.Button();
+            this.btnEşittir = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblSayi = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.btnArtı);
+            this.groupBox1.Controls.Add(this.btnEksi);
+            this.groupBox1.Controls.Add(this.btnÇarpma);
+            this.groupBox1.Controls.Add(this.btnBölme);
+            this.groupBox1.Controls.Add(this.btnEşittir);
+            this.groupBox1.Controls.Add(this.btnC);
+            this.groupBox1.Controls.Add(this.btn0);
+            this.groupBox1.Controls.Add(this.btn9);
+            this.groupBox1.Controls.Add(this.btn8);
+            this.groupBox1.Controls.Add(this.btn7);
+            this.groupBox1.Controls.Add(this.btn6);
+            this.groupBox1.Controls.Add(this.btn5);
+            this.groupBox1.Controls.Add(this.btn4);
+            this.groupBox1.Controls.Add(this.btn3);
+            this.groupBox1.Controls.Add(this.btn2);
+            this.groupBox1.Controls.Add(this.btn1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 164);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(358, 405);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tuş Takımı";
+            // 
+            // btnArtı
+            // 
+            this.btnArtı.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnArtı.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnArtı.Location = new System.Drawing.Point(278, 38);
+            this.btnArtı.Name = "btnArtı";
+            this.btnArtı.Size = new System.Drawing.Size(65, 80);
+            this.btnArtı.TabIndex = 16;
+            this.btnArtı.Text = "+";
+            this.btnArtı.UseVisualStyleBackColor = false;
+            this.btnArtı.Click += new System.EventHandler(this.btnArtı_Click);
+            // 
+            // btnEksi
+            // 
+            this.btnEksi.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnEksi.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEksi.Location = new System.Drawing.Point(278, 124);
+            this.btnEksi.Name = "btnEksi";
+            this.btnEksi.Size = new System.Drawing.Size(65, 80);
+            this.btnEksi.TabIndex = 15;
+            this.btnEksi.Text = "-";
+            this.btnEksi.UseVisualStyleBackColor = false;
+            this.btnEksi.Click += new System.EventHandler(this.btnEksi_Click);
+            // 
+            // btnÇarpma
+            // 
+            this.btnÇarpma.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnÇarpma.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnÇarpma.Location = new System.Drawing.Point(278, 210);
+            this.btnÇarpma.Name = "btnÇarpma";
+            this.btnÇarpma.Size = new System.Drawing.Size(65, 80);
+            this.btnÇarpma.TabIndex = 14;
+            this.btnÇarpma.Text = "*";
+            this.btnÇarpma.UseVisualStyleBackColor = false;
+            this.btnÇarpma.Click += new System.EventHandler(this.btnÇarpma_Click);
+            // 
+            // btnBölme
+            // 
+            this.btnBölme.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnBölme.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBölme.Location = new System.Drawing.Point(278, 296);
+            this.btnBölme.Name = "btnBölme";
+            this.btnBölme.Size = new System.Drawing.Size(65, 80);
+            this.btnBölme.TabIndex = 13;
+            this.btnBölme.Text = "/";
+            this.btnBölme.UseVisualStyleBackColor = false;
+            this.btnBölme.Click += new System.EventHandler(this.btnBölme_Click);
+            // 
+            // btnEşittir
+            // 
+            this.btnEşittir.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnEşittir.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEşittir.Location = new System.Drawing.Point(192, 296);
+            this.btnEşittir.Name = "btnEşittir";
+            this.btnEşittir.Size = new System.Drawing.Size(80, 80);
+            this.btnEşittir.TabIndex = 12;
+            this.btnEşittir.Text = "=";
+            this.btnEşittir.UseVisualStyleBackColor = false;
+            this.btnEşittir.Click += new System.EventHandler(this.btnEşittir_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnC.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC.Location = new System.Drawing.Point(20, 296);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(80, 80);
+            this.btnC.TabIndex = 11;
+            this.btnC.Text = "C";
+            this.btnC.UseVisualStyleBackColor = false;
+            this.btnC.Click += new System.EventHandler(this.btnC_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn0.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(106, 296);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(80, 80);
+            this.btn0.TabIndex = 10;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn9.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(192, 210);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(80, 80);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn8.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(106, 210);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(80, 80);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn7.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(20, 210);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(80, 80);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn6.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(192, 124);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(80, 80);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn5.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(106, 124);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(80, 80);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn4.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(20, 124);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(80, 80);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn3.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(192, 38);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(80, 80);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn2.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(106, 38);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(80, 80);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btn1.Font = new System.Drawing.Font("MV Boli", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(20, 37);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(80, 80);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.groupBox2.Controls.Add(this.lblSayi);
+            this.groupBox2.Location = new System.Drawing.Point(12, 23);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(358, 99);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "sonuç";
+            // 
+            // lblSayi
+            // 
+            this.lblSayi.AutoSize = true;
+            this.lblSayi.Font = new System.Drawing.Font("MV Boli", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSayi.Location = new System.Drawing.Point(6, 17);
+            this.lblSayi.Name = "lblSayi";
+            this.lblSayi.Size = new System.Drawing.Size(76, 79);
+            this.lblSayi.TabIndex = 0;
+            this.lblSayi.Text = "0";
+            this.lblSayi.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(391, 582);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Hesap Makinesi";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btnÇarpma;
+        private System.Windows.Forms.Button btnBölme;
+        private System.Windows.Forms.Button btnEşittir;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btnArtı;
+        private System.Windows.Forms.Button btnEksi;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblSayi;
+    }
+}
+
